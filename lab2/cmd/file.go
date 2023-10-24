6135c0da9e83a22af4423c5851963f2c8014ed66
turnooff <kostim37@gmail.com>
2023-10-24 08:37:13 +0300

    Лабораторная 2

---
+++ lab2/cmd/file.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//Example of tested file
+func findKthLargest(nums []int, k int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(nums))) // sort.Ints(nums) можно использовать прямую сортировку
+	fmt.Println(nums[k-1]) // fmt.Println(nums[len(nums)-k]) в случае прямой сортировки вывод такой
+	return 0
+}
