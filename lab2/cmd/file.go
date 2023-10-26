6e17d68b22e0de7ceb751f90564ddfcbdfd06111
turnooff <112732132+turnooff@users.noreply.github.com>
2023-10-26 18:40:55 +0300

    Задумка не сработала, поэтому вернусь к предыдущему коду:)

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,25 +1,6 @@
 package main
 
 func findKthLargest(nums []int, k int) int {
-	//sort.Sort(sort.Reverse(sort.IntSlice(nums))) // sort.Ints(nums) можно использовать прямую сортировку
-	//return nums[k-1] // nums[len(nums)-k] в случае прямой сортировки вывод такой
-	helpArr := make([]int, k)
-	for i := 0; i < k; i++ {
-		//append(helpArr, nums[i])
-		helpArr[i] = nums[i]
-	}
-	for i := k; i < len(nums); i++ {
-		for j := 0; j < k; j++ {
-			if nums[i] > helpArr[j] {
-				helpArr[j] = nums[i]
-			}
-		}
-	}
-	min := helpArr[0]
-	for i := 1; i < len(helpArr); i++ {
-		if helpArr[i] < min {
-			min = helpArr[i]
-		}
-	}
-	return min
+	sort.Sort(sort.Reverse(sort.IntSlice(nums))) // sort.Ints(nums) можно использовать прямую сортировку
+	return nums[k-1] // nums[len(nums)-k] в случае прямой сортировки вывод такой
 }
