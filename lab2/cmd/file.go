1ee6733a687de362ae9786d2d076dc61b45dedaa
turnooff <kostim37@gmail.com>
2023-10-26 18:29:43 +0300

    Пытаюсь сделать без библиотеки sort

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -5,6 +5,25 @@ import (
 )
 
 func findKthLargest(nums []int, k int) int {
-	sort.Sort(sort.Reverse(sort.IntSlice(nums))) // sort.Ints(nums) можно использовать прямую сортировку
-	return nums[k-1] // nums[len(nums)-k] в случае прямой сортировки вывод такой
+	//sort.Sort(sort.Reverse(sort.IntSlice(nums))) // sort.Ints(nums) можно использовать прямую сортировку
+	//return nums[k-1] // nums[len(nums)-k] в случае прямой сортировки вывод такой
+	helpArr := make([]int, k)
+	for i := 0; i < k; i++ {
+		//append(helpArr, nums[i])
+		helpArr[i] = nums[i]
+	}
+	for i := k; i < len(arr); i++ {
+		for j := 0; j < k; j++ {
+			if nums[i] > helpArr[j] {
+				helpArr[j] = nums[i]
+			}
+		}
+	}
+	min := helpArr[0]
+	for i := 1; i < len(helpArr); i++ {
+		if helpArr[i] < min {
+			min = helpArr[i]
+		}
+	}
+	return min
 }
