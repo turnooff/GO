98ff57ebf615b683b33876896b7040ddd7f9dcdf
turnooff <112732132+turnooff@users.noreply.github.com>
2023-10-26 18:42:23 +0300

    Добавил вызов библиотеки sort

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 func findKthLargest(nums []int, k int) int {
 	sort.Sort(sort.Reverse(sort.IntSlice(nums))) // sort.Ints(nums) можно использовать прямую сортировку
 	return nums[k-1] // nums[len(nums)-k] в случае прямой сортировки вывод такой
