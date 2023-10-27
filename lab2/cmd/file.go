204cca6dd342622d463de80ca50e7a1ff72ee3b0
turnooff <112732132+turnooff@users.noreply.github.com>
2023-10-27 07:06:08 +0300

    Разделил код на логические функции

--- lab2/cmd/file.go
+++ lab2/cmd/file.go
@@ -1,13 +1,6 @@
 package main
 
-
-func findKthLargest(nums []int, k int) int {
-	
-	//Изначально использовал встроенную библиотеку sort
-	//sort.Sort(sort.Reverse(sort.IntSlice(nums)))
-	//return nums[k-1]
-	//но потом решил написать сортировку вручную
-
+func shellSort(arr []int) []int {
 	var gap, i int
 	for gap = len(nums) / 2; gap > 0; gap /= 2 {
 		for i = gap; i < len(nums); i++ {
@@ -19,5 +12,16 @@ func findKthLargest(nums []int, k int) int {
 			nums[j] = temp
 		}
 	}
+	return arr
+}
+
+func findKthLargest(nums []int, k int) int {
+	
+	//Изначально использовал встроенную библиотеку sort
+	//sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	//return nums[k-1]
+	//но потом решил написать сортировку вручную
+
+	nums = shellSort(nums)
 	return nums[len(nums)-k]
 }
